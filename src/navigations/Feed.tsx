98ad6597a162ec21c8a03001855f08a05cfc6a80
Ata Can Çalışkan <caliskanataa@gmail.com>
2023-04-25 14:30:15 +0300

    Second

--- src/navigations/Feed.tsx
+++ src/navigations/Feed.tsx
@@ -2,15 +2,39 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Weekly from "../screens/main/Weekly";
 import SeparateTasks from "../screens/main/SeparateTasks";
 import Statistics from "../screens/main/Statistics";
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator();
 
 const FeedNavigation = () => {
     return (
         <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}
+            screenOptions={({ route }) => ({
+                    headerShown: false,
+                    tabBarStyle: {
+                        backgroundColor: '#333',
+                        paddingVertical: 10
+                    },
+                    tabBarIcon: ({ focused, color, size }) =>{
+                        let iconName;
+
+                        if (route.name === 'Weekly') {
+                            iconName = focused
+                              ? 'ios-calendar'
+                              : 'ios-calendar-outline';
+                        } else if (route.name === 'SeparateTasks') {
+                            iconName = focused ? 'ios-list' : 'ios-list-outline';
+                        } else if (route.name === 'Statistics') {
+                            iconName = focused ? 'ios-analytics-sharp' : 'ios-analytics-outline';
+                        }
+              
+                          // You can return any component that you like here!
+                        return <Icon name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })
+            }
         >
             <Tab.Screen
                 name="Weekly"
